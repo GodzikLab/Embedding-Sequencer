342faefd4aeac40eb6c9203d8c907bc8244af702
Keita Ichii <100912526+kichi004@users.noreply.github.com>
2025-02-04 17:04:31 -0800

    create scripting.py

---
+++ scripting.py
@@ -0,0 +1,27 @@
+# timer?
+
+# parse input, build input list, and determine input/output
+
+# extract information from HDF file
+
+# build faiss index
+
+# establish/download model
+
+# LOOP - start output list
+
+    # generate embeddings, normalize
+
+    # perform ANN with N neighbors
+
+    # determine cluster-label sequence
+
+    # calculate outlier percentage
+
+    # find repeat locations
+
+    # save to output list
+
+# END LOOP
+
+# mirror output: single fasta, multiple fastas, csv/tsv files
