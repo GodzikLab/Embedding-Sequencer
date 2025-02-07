94192942e605aaef08025974744eb1c2437ffab3
Keita Ichii <100912526+kichi004@users.noreply.github.com>
2025-02-06 16:33:19 -0800

    added input processing for script

--- scripting.py
+++ scripting.py
@@ -1,27 +1,60 @@
-# timer?
+# Used to script actions embedding sequencer.
 
-# parse input, build input list, and determine input/output
+import modules.cluster_mapping
+import modules.embedding_generation
+import modules.file_io
 
-# extract information from HDF file
+import time
 
-# build faiss index
+# inputs to consider
+'''
+- output type: fasta, aln, tsv (flags -f, -a, -t)
+    - determine whether to call the directory function based on the number of inputs
+- print flag: -p 
+- path input > gets processed in the scripting (there's a function)
+- path output (optional) - change the name of the output single file
 
-# establish/download model
+main scripting function(input, output_type = f/a/t, print = True/False, output = "")
+'''
+def run_pipeline(input_path, output_type = "f", print = False, output = ""):
+    '''Manages the scripting of actions for the embedding sequencer pipeline.'''
+    # timer
+    start_time = round(time.perf_counter(), 4)
 
-# LOOP - start output list
+    # parse input, build input list, and determine input/output
+    input_type = modules.file_io.determine_input(input_path) # 0 - FASTA, 1 - directory, 2 - CSV/TSV
+    if input_type == 0:
+        input_df = modules.file_io.read_fasta(input_path)
+    elif input_type == 1:
+        input_df = modules.file_io.read_directory_fastas(input_path)
+    elif input_type == 2:
+        input_df = modules.file_io.read_csv_tsv(input_path)
+    else:
+        raise ValueError("Input is not an acceptable format. Please enter a path for a FASTA, directory of FASTAs, or CSV/TSV.")
 
-    # generate embeddings, normalize
+    # extract information from HDF file
+    
 
-    # perform ANN with N neighbors
+    # build faiss index
 
-    # determine cluster-label sequence
+    # establish/download model
 
-    # calculate outlier percentage
+    # LOOP - start output list
 
-    # find repeat locations
+        # generate embeddings, normalize
 
-    # save to output list
+        # perform ANN with N neighbors
 
-# END LOOP
+        # determine cluster-label sequence
 
-# mirror output: single fasta, multiple fastas, csv/tsv files
+        # calculate outlier percentage
+
+        # find repeat locations
+
+        # save to output list
+
+    # END LOOP
+
+    # mirror output: single fasta, multiple fastas, csv/tsv files
+
+    return
