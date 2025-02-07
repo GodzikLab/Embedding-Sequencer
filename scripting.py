5c6c4865f43ec64bc8b5273bd9d40ecf43f5a653
Keita Ichii <100912526+kichi004@users.noreply.github.com>
2025-02-06 17:20:06 -0800

    finished scripting.py

--- scripting.py
+++ scripting.py
@@ -5,6 +5,7 @@ import modules.embedding_generation
 import modules.file_io
 
 import time
+import pandas as pd
 
 # inputs to consider
 '''
@@ -16,8 +17,18 @@ import time
 
 main scripting function(input, output_type = f/a/t, print = True/False, output = "")
 '''
-def run_pipeline(input_path, output_type = "f", print = False, output = ""):
-    '''Manages the scripting of actions for the embedding sequencer pipeline.'''
+def run_pipeline(input_path, hdf_file = "20241205_hTLR_pool.hdf", output_type = 1, print_flag = False, output_path = "fasta_output"):
+    '''Manages the scripting of actions for the embedding sequencer pipeline.
+    
+    Parameters:
+        input_path: Input of the pipeline, takes FASTA, directory, or CSV/TSV
+        hdf_file: Clustering mapping information as an HDF file
+        output_type: Determines the output of the pipeline (1 - FASTA, 2 - ALN, 3 - TSV file)
+        print_flag: If True, prints progress updates within the pipeline.
+        output_path: Determines the output folder
+    '''
+    if print_flag: print("Starting Embedding Sequencer Pipeline...\n")
+
     # timer
     start_time = round(time.perf_counter(), 4)
 
@@ -31,30 +42,68 @@ def run_pipeline(input_path, output_type = "f", print = False, output = ""):
         input_df = modules.file_io.read_csv_tsv(input_path)
     else:
         raise ValueError("Input is not an acceptable format. Please enter a path for a FASTA, directory of FASTAs, or CSV/TSV.")
-
-    # extract information from HDF file
     
+    # check input and output_type
+    modules.file_io.validate_input_df(input_df) # checks df for columns and non-empty
+    if print_flag: print(f"Successfully inputted query of {len(input_df)} proteins.")
 
-    # build faiss index
+    # extract clustering information from HDF file and build FAISS index
+    aggregated_embeddings, cluster_labels, _, indicative_pattern, _ = modules.cluster_mapping.unpack_hdf(hdf_file)
+    faiss_index = modules.cluster_mapping.build_faiss_index(aggregated_embeddings)
+    if print_flag: print(f"Unpacked clustering information from {hdf_file} and built FAISS index.")
 
     # establish/download model
+    model, batch_converter = modules.embedding_generation.download_model()
 
-    # LOOP - start output list
+    # start output list
+    output_data = []
+    num_neighbors = 50
 
-        # generate embeddings, normalize
+    # LOOP
+    for _, row in input_df.iterrows():
+        name, sequence = row["Entry Name"], row["Sequence"]
+
+        # generate embeddings
+        query_embeddings = modules.embedding_generation.generate_embeddings(sequence, model, batch_converter)
 
         # perform ANN with N neighbors
-
+        faiss_similarity, faiss_indices = modules.cluster_mapping.search_faiss_index(query_embeddings, faiss_index, num_neighbors = num_neighbors)
+        
         # determine cluster-label sequence
+        query_sequence, outlier_dict = modules.cluster_mapping.build_faiss_sequence(faiss_similarity, faiss_indices, cluster_labels, num_neighbors = num_neighbors)
 
         # calculate outlier percentage
+        sequence_confidence = modules.cluster_mapping.calculate_confidence(outlier_dict, faiss_similarity)
 
         # find repeat locations
+        pattern_indexes = modules.embedding_generation.find_pattern(query_sequence, indicative_pattern)
+        if pattern_indexes: # check if pattern indexes if empty
+            start_pos, end_pos = pattern_indexes[0], pattern_indexes[-1]
+        else:
+            start_pos, end_pos = -1, -1
 
         # save to output list
+        data_entry = {
+            "Entry Name" : name,
+            "Sequence" : query_sequence,
+            "Sequence Confidence" : sequence_confidence,
+            "Repeat Locations" : pattern_indexes,
+            "Number of Repeats" : len(pattern_indexes),
+            "Start" : start_pos,
+            "End" : end_pos
+        }
+        output_data.append(data_entry)
 
-    # END LOOP
+    output_df = pd.DataFrame(output_data)
 
-    # mirror output: single fasta, multiple fastas, csv/tsv files
+    # output
+    if output_type == 1: # FASTA
+        modules.file_io.write_fastas_to_directory(output_df, output_directory = output_path)
+    elif output_type == 2: # ALN
+        modules.file_io.write_alns_to_directory(output_df, output_directory = output_path)
+    elif output_type == 3: # TSV
+        modules.file_io.write_tsv(output_df)
+    else:
+        raise ValueError("Invalid output type entered.")
 
     return
